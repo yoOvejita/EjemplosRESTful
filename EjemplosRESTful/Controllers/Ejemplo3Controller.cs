7a48d73dbe4d996a06728b25f24b1c934d40b9bf
Rubén Gonzalo Soria Soria <rusok.trabajo@hotmail.com>
2022-07-21 20:49:51 -0400

    Ejemplo 3

---
+++ EjemplosRESTful/Controllers/Ejemplo3Controller.cs
@@ -0,0 +1,60 @@
+﻿using EjemplosRESTful.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace EjemplosRESTful.Controllers
+{
+    [Route("api/ejemplo3")]
+    [ApiController]
+    public class Ejemplo3Controller : ControllerBase
+    {
+        [Consumes("application/json")]
+        [Produces("application/json")]
+        [HttpPost("prueba1")]
+        public async Task<IActionResult> prueba1([FromBody] Producto prod)
+        {
+            try
+            {
+                prod.nombre += " [AGOTADO]";
+                return Ok(prod);
+            }
+            catch
+            {
+                return BadRequest();
+            }
+
+        }
+
+        [Consumes("application/json")]
+        [Produces("application/json")]
+        [HttpPut("prueba2")]
+        public async Task<IActionResult> prueba2([FromBody] Producto prod)
+        {
+            try
+            {
+                prod.nombre += " [OFERTA -20%]";
+                return Ok(prod);
+            }
+            catch
+            {
+                return BadRequest();
+            }
+
+        }
+        [HttpDelete("prueba3/{id}")]
+        public async Task<IActionResult> prueba3(string id)
+        {
+            try
+            {
+                //Debug.WriteLine("");
+                return Ok(id);
+            }
+            catch
+            {
+                return BadRequest();
+            }
+
+        }
+    }
+}
